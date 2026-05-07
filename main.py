0fa2e9a7fd8c28d882c1828c98ed40e3b70ffc77
Tellamsaikrishna27 <143862233+Tellamsaikrishna27@users.noreply.github.com>
2026-05-07 21:26:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from flask import Flask
 import threading
 import time
-
 app = Flask(name)
-
 @app.route('/')
 def home():
     return "AI Trading Bot Running"
-
 def bot_loop():
     while True:
         print("Bot Running...")
         time.sleep(60)
-
 threading.Thread(target=bot_loop).start()
-
 if name == "main":
     app.run(host="0.0.0.0", port=10000)
